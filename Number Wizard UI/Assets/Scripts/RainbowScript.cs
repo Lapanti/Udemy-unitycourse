aa593ad7831988597e9ad07a2f34ce6ecce37633
Lapanti <laurilavanti@gmail.com>
2017-03-05 20:35:27 +0200

    Rainbow colors in the header

---
+++ Number Wizard UI/Assets/Scripts/RainbowScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RainbowScript : MonoBehaviour {
+	public Text text;
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		text.color = Color.HSVToRGB (Mathf.PingPong (Time.time * speed, 1), 1, 1);
+	}
+}
