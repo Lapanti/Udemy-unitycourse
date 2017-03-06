67ff0ca34802866195b0c5687bfa341db9fc7830
Lapanti <laurilavanti@gmail.com>
2017-03-06 21:07:11 +0200

    Launching ball

--- Block Breaker/Assets/Scripts/Paddle.cs
+++ Block Breaker/Assets/Scripts/Paddle.cs
@@ -3,14 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Paddle : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		this.transform.position = new Vector3(
+			Mathf.Clamp (Input.mousePosition.x / Screen.width * 16, 0.5f, 15.5f),
+			this.transform.position.y,
+			this.transform.position.z
+		);
 	}
 }
