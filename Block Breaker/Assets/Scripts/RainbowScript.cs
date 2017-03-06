f7a589381c81d1ab6e40182ddb54ed0ab47b2f15
Lapanti <laurilavanti@gmail.com>
2017-03-06 19:33:44 +0200

    Working navigation

---
+++ Block Breaker/Assets/Scripts/RainbowScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RainbowScript : MonoBehaviour {
+	public Text text;
+	public float speed;
+	
+	// Update is called once per frame
+	void Update () {
+		text.color = Color.HSVToRGB (Mathf.PingPong (Time.time * speed, 1), 1, 1);
+	}
+}
